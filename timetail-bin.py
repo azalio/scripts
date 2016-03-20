26e5241d801c1213ba2ddfe751d8662e0baa8a8f
azalio <azalio@yandex-team.ru>
2016-03-20 14:52:58 +0300

    bin search v3

--- timetail-bin.py
+++ timetail-bin.py
@@ -6,12 +6,13 @@ import os
 import time
 import sys
 
+
 def parseArgs():
     parser = argparse.ArgumentParser(description="Output some part of file \
                                      for given time intervals")
     parser.add_argument("filepath", type=str, help="Path to file.")
     parser.add_argument("--time", type=int, help="time in second from end of \
-                        the file", default=10)
+                        the file", default=600)
     parser.add_argument("--begin", type=str, help="Start time.")
     parser.add_argument("--end", type=str, help="End time.")
     global args
@@ -23,20 +24,13 @@ def find_point(f, now, filepath):
     os.environ['TZ'] = 'Europe/Moscow'
     time.tzset()
     time_to_find = now - args.time
-    print time.ctime(time_to_find)
+#    print time.ctime(time_to_find)
     year = time.strftime("%Y")
     fSize = os.path.getsize(filepath)
     begin = 0.0
     end = float(fSize)
-#    print "begin = {}, end = {}".format(begin, end)
     while end - begin > 1:
         point_to_seek = begin + (end - begin)/2
-        print "begin = {}, end = {}, point_to_seek = {}".format(begin, \
-                                                                end, \
-                                                                point_to_seek)
-#        time.sleep(1)
-#        print "{}".format(point_to_seek)
-
         f.seek(int(point_to_seek))
         line = f.readline()
         time_in_log = line[0:15]
@@ -46,29 +40,27 @@ def find_point(f, now, filepath):
                                            year,
                                            "%b %d %H:%M:%S %Y")
         except:
-#            point_to_seek = point_to_seek - 256
-#            f.seek(int(point_to_seek))
-            seek = find_new_line_pos(f,f.tell())
-            f.seek(seek)
-            filePos = f.tell()
-            line = f.readline()
-            time_in_log = line[0:15]
-            time_to_struct = time.strptime(time_in_log + " " +
-                                           year,
-                                           "%b %d %H:%M:%S %Y")
+            try:
+                seek = find_new_line_pos(f, f.tell())
+                f.seek(seek)
+                filePos = f.tell()
+                line = f.readline()
+                time_in_log = line[0:15]
+                time_to_struct = time.strptime(time_in_log + " " +
+                                               year,
+                                               "%b %d %H:%M:%S %Y")
+            except:
+                return None
         time_to_unixtime = time.mktime(time_to_struct)
-#        print "{}".format(time_to_find)
-#        print "{}".format(time.ctime(time_to_unixtime))
-#        TODO: Тут надо сравнивать без милисекунд
         if time_to_unixtime > time_to_find:
             end = point_to_seek
-#            print "time_in_log = {}".format(time_in_log)
         elif time_to_unixtime == time_to_find:
-            print "Yehhooo"
-            return filePos
+            newfilePos = check_file_for_date(f, filePos, time_to_find)
+            return newfilePos
         else:
             begin = point_to_seek
     return filePos
+
 
 def find_new_line_pos(f, offset):
     char = ''
@@ -78,9 +70,26 @@ def find_new_line_pos(f, offset):
         char = f.read(1)
     return offset+1
 
-# @profile
+
+def check_file_for_date(f, offset, time_to_find):
+    offset = find_new_line_pos(f, offset-1)
+    f.seek(offset)
+    line = f.readline()
+    newDate = line[0:15]
+    year = time.strftime("%Y")
+    time_to_struct = time.strptime(newDate + " " +
+                                   year,
+                                   "%b %d %H:%M:%S %Y")
+    time_to_unixtime = time.mktime(time_to_struct)
+    if time_to_unixtime == time_to_find:
+        return check_file_for_date(f, offset, time_to_unixtime)
+    else:
+        filePos = f.tell()
+        return filePos
+
+
 def main():
-    now = time.time()
+    now = int(time.time())
     args = parseArgs()
     filepath = args.filepath
     if os.path.exists(filepath):
@@ -92,6 +101,9 @@ def main():
             time_begin_in_unixtime = time.mktime(time_begin)
             if (now - time_begin_in_unixtime > args.time):
                 filePos = find_point(f, now, filepath)
+                if not filePos:
+                    print "no lines"
+                    sys.exit(1)
                 f.seek(filePos)
                 while True:
                     line = f.readline()
@@ -99,7 +111,6 @@ def main():
                         break
                     else:
                         line = str.strip(line)
-#                        print "{}".format(line)
                         try:
                             print line
                         except IOError:
@@ -121,7 +132,6 @@ def main():
                     else:
                         line = str.strip(line)
                         print line
-#                        print "{}".format(line)
     else:
         print "{} is not exist".format(filepath)
         sys.exit(1)
